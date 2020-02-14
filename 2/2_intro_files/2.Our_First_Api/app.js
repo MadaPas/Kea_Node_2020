dcec326b705a2e9a35ff90e86e384e0542020cc5
madapas <mada0193@stud.kea.dk>
2020-02-14 14:01:10 +0100

    api practice

--- 2/2_intro_files/2.Our_First_Api/app.js
+++ 2/2_intro_files/2.Our_First_Api/app.js
@@ -2,9 +2,67 @@ var express = require("express");
 //instanciating
 var app = express();
 
-//or
+//or shorter version
 //var app = require("express")();
 
+//path   //callback function (called back at the discretion of the server)
+app.get("/", (req, res) => {
+    const response = {
+        message: "Hello there!"
+        }
+        res.send(response);
+});
 
 
-app.listen(3000);
+
+//Assignment one: Create an aboutMe route that sends back info about yourself.
+app.get("/aboutMe", (req, res) => {
+    const me = {
+        name: "MadaPas"
+    };
+    res.send(me);
+});
+
+
+
+
+
+
+
+var weekdays = new Array(7);
+weekdays[0] = "Sunday";
+weekdays[1] = "Monday";
+weekdays[2] = "Tuesday";
+weekdays[3] = "Wednesday";
+weekdays[4] = "Thursday";
+weekdays[5] = "Friday";
+weekdays[6] = "Saturday";
+
+
+
+
+app.get("/time", (req, res) => {
+
+    const date = new Date();
+    weekday = date.getDay();
+  
+    res.send({
+        unformatedTime: date,
+        time: date.toString(),
+        day: date.getDay(),
+        weekday: weekdays[weekday]
+    });
+});
+
+
+// app.listen(3000);
+
+app.listen(3000, error => {
+    if (error) {
+        console.log("Error running the server", error);
+    }
+    console.log("Server is running on port ", 3000);
+});
+
+
+
--- 2/2_intro_files/firstFile.js
+++ 2/2_intro_files/firstFile.js
@@ -6,3 +6,45 @@ console.log("Hello,", name);
 console.log(name, "is", age, "years old! WOW")
 
 var anotherName = 'Elf';
+
+
+
+/////////////////////////////////////////////////
+const app = require("express")(); 
+
+
+app.get("/", (req, res) => {
+    res.send({ message: "Hello there" });
+});
+
+// define something on the path /aboutMe that returns a JSON representation of you
+
+app.get("/aboutMe", (req, res) => {
+    const me = {
+        name: "Radu"
+    };
+    res.send(me);
+});
+
+// define something on the path /aboutThisWebsite 
+// that returns a JSON representation of the website
+app.get("/aboutThisWebsite", (req, res) => {
+    const aboutThisWebsite = {
+        name: "thiswebsiteIdunno"
+    };
+    //if (aboutThisWebsite) {
+        res.send(aboutThisWebsite);
+    // }
+    // res.send("Sorry, no info about this website.");
+    // this here won't run
+});
+
+
+
+
+app.listen(3000, error => {
+    if (error) {
+        console.log(error);
+    }
+    console.log("Server is running on port", 3000);
+});
